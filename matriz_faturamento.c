4d4fec7e63a6b7e8aa49f2abb9613c1042f2966b
makalau <marcelovictor95@hotmail.com>
2022-03-22 22:23:20 -0300

    Matriz para representar um pseudo-faturamento.

--- matriz_faturamento.c
+++ matriz_faturamento.c
@@ -3,24 +3,37 @@
 #include <stdio.h>
 #include <locale.h>
 
+float faturamento[3][4];
+char meses[13][13] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+
+void entrada_faturamento()
+{
+  for (int indice1=0; indice1 < 3; indice1++)
+  {
+    printf("-------------------------------------------------------\n");
+    for (int indice2=0; indice2 < 4; indice2++)
+    {
+      printf("Digite o faturamento da %dª semana no mês de %s: ", indice2+1, meses[indice1]);
+      scanf("%f", &faturamento[indice1][indice2]);
+    }
+  }
+}
+
+
 int main(void){
   setlocale(LC_ALL, "Portuguese");
-  float faturamento[3][4] = {
-    {150000, 98000, 120000,75000},
-    {120000, 100000, 70000, 66000},
-    {75000, 180000, 95000, 120000}
-  };
+  entrada_faturamento();
   printf("Bem vindo ao programa de controle dos seus indicadores financeiros.");
   for (int i_a = 0; i_a < 3; i_a++)
     {
 
       printf("\n");
-      printf("\n*-------- Faturamento no %dº mês: --------*", i_a+1);
+      printf("\n*-------- Faturamento no mês de %s: --------*", meses[i_a]);
 
     for (int i_b = 0; i_b < 4; i_b++)
       {
         printf("\n    %dª Semana lucro de: R$ %2.f,00", i_b+1,faturamento[i_a][i_b]);
       }
     }
-	return 0;
+  return 0;
 }
